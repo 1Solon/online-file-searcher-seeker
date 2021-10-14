6a27ee828f327580c9cd7724adcc9b66fcb64f25
Saul Burgess <Burgesssaul@gmail.com>
2021-10-14 13:14:36 +0100

    Adds serverside code

---
+++ server/server.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+
+var corsOptions = {
+    origin: process.env.CLIENT_ORIGIN || "http://localhost:8081"
+  };
+
+app.use(cors(corsOptions));
+
+// set port, listen for requests
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
