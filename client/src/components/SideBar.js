64c811deff27c44dae05f6e38bad8365cf0f75be
Saul Burgess <Burgesssaul@gmail.com>
2021-10-21 15:16:36 +0100

    Colab Session #01

---
+++ client/src/components/SideBar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './styles/HomePage.css';
+import axios from 'axios';
+import { Button, Container, Card, Row } from 'react-bootstrap'
+
+// Links boxicons
+{/* <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'/> */}
+
+class SideBar extends Component {
+  render() {
+    return <h2>Hi, I am Abraham</h2>;
+  } 
+}
+
+// ReactDOM.render(<SideBar />, document.getElementById('root')); // has to be in the index
+
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Renders App.js 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<HomePage />, document.getElementById('root'));
+// ReactDOM.render(<Login />, document.getElementById('root'));
