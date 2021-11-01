288953ee22ca3493b4b5a53cdcbf2244c9ad6eb2
Nicolas <nicoalavarezgarrido@gmail.com>
2021-11-01 12:41:18 +0000

    app.js deleted

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// import HomePage from './components/HomePage';
-// import Login from './components/Login';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './components/HomePage';
+import Login from './components/Login';
+// import SideBar from './components/SideBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Renders App.js 
-// ReactDOM.render(<HomePage />, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HomePage />, document.getElementById('root'));
+// ReactDOM.render(<Login />, document.getgetElementById('login'));
