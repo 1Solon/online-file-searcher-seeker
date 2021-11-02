b5c07d902086c872d1b1e252d664eff7b5f8b6fc
abrahax01 <c19340801@mytudublin.ie>
2021-11-02 23:17:29 +0000

    Css fixed
    Fixed issue with having multiple CSS files by using grouping (adding classNames/IDs to tags and changing css accordingly

--- client/src/index.js
+++ client/src/index.js
@@ -7,4 +7,4 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Renders App.js 
 ReactDOM.render(<HomePage />, document.getElementById('root'));
-// ReactDOM.render(<Login />, document.getgetElementById('login'));
+// ReactDOM.render(<Login />, document.getElementById('root'));
