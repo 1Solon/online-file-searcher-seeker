7623ef0df2fbefcf83b6bfe82f5849660d2a445b
Saul Burgess <Burgesssaul@gmail.com>
2021-11-04 14:44:06 +0000

    Removes ';' to comply with new styleguide

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 
-const express = require('express');
-const mysql = require('mysql2');
-const cors = require('cors');
-const app = express();
+const express = require('express')
+const mysql = require('mysql2')
+const cors = require('cors')
+const app = express()
 
-const bcrypt = require('bcrypt'); // Password incription -> For t his to work install npm install bcrypt inside the api container
+const bcrypt = require('bcrypt') // Password incription -> For t his to work install npm install bcrypt inside the api container
 const saltRounds = 10
 
 
@@ -21,18 +21,18 @@ app.use(cors())
 
 // Starts our express server
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // Sets a response on our server, so we can test if the server is alive or not
 app.get('/', (req, res) => {
   res.send('I am alive!')
-});
+})
 
 // Handles adding a user to the database to the DB -> Registering user + encrypting password
 app.post("/register", (req, res) => {
-  const setUserName = req.body.setUsername;
-  const setEmail = req.body.setEmail;
-  const setPassword = req.body.setPassword;
+  const setUserName = req.body.setUsername
+  const setEmail = req.body.setEmail
+  const setPassword = req.body.setPassword
 
   // Password encryption
   bcrypt.hash(setPassword, saltRounds, (err, hash) => {
@@ -46,10 +46,10 @@ app.post("/register", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
-  const setUserName = req.query.setUserName;
-  const setPassword = req.query.setPassword;
+  const setUserName = req.query.setUserName
+  const setPassword = req.query.setPassword
 
-  db.query("SELECT * FROM USERS WHERE USER_NAME = ?;", setUserName, (err, result) => {
+  db.query("SELECT * FROM USERS WHERE USER_NAME = ?", setUserName, (err, result) => {
     if(result.length > 0) {
       bcrypt.compare(setPassword, result[0].password, (error, response) => {
         if(response){
@@ -68,18 +68,18 @@ app.post("/login", (req, res) => {
 
 // Handles deleting an element from the DB
 // app.delete("/delete/:bookId", (req, res) => {
-//   const bookId = req.params.bookId;
-//   const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?";
+//   const bookId = req.params.bookId
+//   const DeleteQuery = "DELETE FROM books_reviews WHERE id = ?"
 //   db.query(DeleteQuery, bookId, (err, result) => {
-//     if (err) console.log(err);
+//     if (err) console.log(err)
 //   })
 // })
 
 // Handles updating an element from the DB
 // app.put("/update/:bookId", (req, res) => {
-//   const bookReview = req.body.reviewUpdate;
-//   const bookId = req.params.bookId;
-//   const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?";
+//   const bookReview = req.body.reviewUpdate
+//   const bookId = req.params.bookId
+//   const UpdateQuery = "UPDATE books_reviews SET book_review = ? WHERE id = ?"
 //   db.query(UpdateQuery, [bookReview, bookId], (err, result) => {
 //     if (err) console.log(err)
 //   })
